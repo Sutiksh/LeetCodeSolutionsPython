42a4940963dbe0d8d230f310676ee6fc123e0872
Sutiksh Verma <verma.sutiksh27@gmail.com>
2022-02-16 17:30:00 -0500

    Time: 43 ms (49.64%), Space: 13.8 MB (91.68%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dictt={"(":")","{":"}","[":"]"}
+        for i in s:
+            if i in dictt:
+                stack.append(i)
+            elif stack==[] or dictt[stack.pop()]!=i:
+                return False
+        return stack==[]
+                
