3ac6ba58f7a2b57852424121c249c0a2e5b81d3d
Sutiksh Verma <verma.sutiksh27@gmail.com>
2022-04-04 14:12:53 -0400

    Time: 95 ms (59.23%), Space: 15.2 MB (51.49%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm = {}
+        hm = {} # Create hm for (num, index)
         for i in range(len(nums)):
-            comp = target - nums[i]
-            if comp in hm:
-                return [hm[comp], i]
+            comp = target - nums[i] # take difference 
+            if comp in hm: # check if difference exist in hm
+                return [hm[comp], i] # return index of difference and current
             else:
-                hm[nums[i]] = i
+                hm[nums[i]] = i # else add to hm
         
         return []
         
